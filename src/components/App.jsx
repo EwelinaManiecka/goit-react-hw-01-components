a06913b0072277a0cd010cb871c918a17fac2b93
EwelinaManiecka <105506837+EwelinaManiecka@users.noreply.github.com>
2022-12-13 16:31:22 +0100

    all files
    add PropTypes
    format all files with Prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import Profile from "./Profile/Profile";
-import user from "../data/user.json";
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
 
-import Statistics from "./Statistics/Statistics";
-import data from "../data/data.json";
+import Statistics from './Statistics/Statistics';
+import data from '../data/data.json';
 
-import FriendList from "./FriendList/FriendList";
-import friends from "../data/friends.json";
+import FriendList from './FriendList/FriendList';
+import friends from '../data/friends.json';
 
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-import transactions from "../data/transactions.json";
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 const App = () => {
   return (
@@ -18,7 +18,6 @@ const App = () => {
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </>
-
   );
 };
 
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,35 +1,41 @@
-import PropTypes from "prop-types";
-import css from "./Statistics.module.css";
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
 
-const bgColor = () => "#" + Math.floor(Math.random() * 16777215).toString(16);
+const bgColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-const Statistics = ({stats, title}) => {
-    return (
-        <section className={css.statistics}>
-        <div className={css.stats__container}>
-            {title && <h2 className={css.title}>{title}</h2>}
-            <ul className={css.stat__list}>
-                {stats.map((stat => {
-                    return <li className={css.item} style={{backgroundColor: bgColor()}} key={stat.id}>
-                    <span className={css.label}>{stat.label}</span>
-                    <span className={css.percentage}>{stat.percentage}%</span>
-                </li>
-                }))}
-            </ul>
-        </div>
-        </section>
-    )
-}
+const Statistics = ({ stats, title }) => {
+  return (
+    <section className={css.statistics}>
+      <div className={css.stats__container}>
+        {title && <h2 className={css.title}>{title}</h2>}
+        <ul className={css.stat__list}>
+          {stats.map(stat => {
+            return (
+              <li
+                className={css.item}
+                style={{ backgroundColor: bgColor() }}
+                key={stat.id}
+              >
+                <span className={css.label}>{stat.label}</span>
+                <span className={css.percentage}>{stat.percentage}%</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 Statistics.propTypes = {
-    title: PropTypes.string.isRequired,
-    stats: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            label: PropTypes.string.isRequired,
-            percentage: PropTypes.number.isRequired,
-        })
-    )
-}
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default Statistics;
